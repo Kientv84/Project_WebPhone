9591d1ed37c44b80675c06623ba7f95f07fb9828
Admin <dinhtrieu.15062002@gmail.com>
2023-10-21 11:25:26 +0700

    update

--- src/redux/slice/userslice.js
+++ src/redux/slice/userslice.js
@@ -1,27 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
 const initialState = {
   name: '',
   email: '',
-  access_token: ''
+  access_token: '',
+    
 }
 
-export const userSlice = createSlice({
+export const userSlide = createSlice({
   name: 'user',
   initialState,
   reducers: {
     updateUser: (state, action) => {
-      const { name, email, access_token} = action.payload
-      // console.log('action', action)
-      state.name = name || email;
+      const {name, email, access_token} = action.payload
+            state.name = name || email;
       state.email = email;
-      state.access_token = access_token
+      state.access_token = access_token;
+        },
+        resetUser: (state) => {
+            state.name = '';
+            state.email = '';
+            state.access_token = '';
     },
   },
 })
 
-// Action creators are generated for each case reducer function
-export const { updateUser } = userSlice.actions
-
-export default userSlice.reducer
+export const {updateUser, resetUser} =userSlide.actions
