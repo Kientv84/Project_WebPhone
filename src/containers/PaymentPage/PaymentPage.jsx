cd2defb0c826b6d05d8c6f63f97a91212b802de9
Admin <dinhtrieu.15062002@gmail.com>
2024-10-07 15:27:14 +0700

    fix

--- src/containers/PaymentPage/PaymentPage.jsx
+++ src/containers/PaymentPage/PaymentPage.jsx
@@ -139,57 +139,49 @@ const PaymentPage = () => {
       });
     }
   };
-const handleQrCodePayment = () => {
-  setIsOpenModalQRcode(true); // Mở modal QR code
+  const handleQrCodePayment = () => {
+    setIsOpenModalQRcode(true); // Mở modal QR code
 
-  let timeLeft = 300; // 15 phút = 900 giây
-  const interval = setInterval(async () => {
-    if (timeLeft > 0) {
-      try {
-        // Gọi hàm checkPaid để kiểm tra thanh toán
-        const paymentSuccess = await checkPaid(totalPriceMemo, content);
+    let timeLeft = 300; // 15 phút = 900 giây
+    const interval = setInterval(async () => {
+      if (timeLeft > 0) {
+        try {
+          // Gọi hàm checkPaid để kiểm tra thanh toán
+          const paymentSuccess = await checkPaid(totalPriceMemo, content);
 
-        if (paymentSuccess) {
-          clearInterval(interval); // Dừng interval nếu thanh toán thành công
-          message.success(t('PAYMENT.PAY_SUCCESS'));
+          if (paymentSuccess) {
+            clearInterval(interval); // Dừng interval nếu thanh toán thành công
+            message.success(t("PAYMENT.PAY_SUCCESS"));
 
-          setIsOpenModalQRcode(false); // Đóng modal sau khi thanh toán thành công
+            setIsOpenModalQRcode(false); // Đóng modal sau khi thanh toán thành công
 
-          mutationAddOrder.mutate({
-          token: user?.access_token,
-          orderItems: order?.orderItemsSelected,
-          fullName: user?.name,
-          address: user?.address,
-          phone: user?.phone,
-          city: user?.city,
-          paymentMethod: "qr_code",
-          itemsPrice: priceMemo,
-          shippingPrice: deliveryPriceMemo,
-          totalPrice: totalPriceMemo,
-          user: user?.id,
-          email: user?.email,
-          isPaid: true,  
-         });
+            mutationAddOrder.mutate({
+              token: user?.access_token,
+              orderItems: order?.orderItemsSelected,
+              fullName: user?.name,
+              address: user?.address,
+              phone: user?.phone,
+              city: user?.city,
+              paymentMethod: "qr_code",
+              itemsPrice: priceMemo,
+              shippingPrice: deliveryPriceMemo,
+              totalPrice: totalPriceMemo,
+              user: user?.id,
+              email: user?.email,
+              isPaid: true,
+            });
+          }
+        } catch (error) {
+          console.error("Lỗi khi kiểm tra thanh toán:", error);
         }
-      } catch (error) {
-        console.error("Lỗi khi kiểm tra thanh toán:", error);
-      }
 
-      timeLeft -= 3; // Cập nhật thời gian còn lại (2 giây mỗi lần)
-    } else {
-      clearInterval(interval); // Hết thời gian, dừng việc kiểm tra
-      message.error(t('PAYMENT.PAY_QR_CODE_FAIL'));
-      setIsOpenModalQRcode(false); // Đóng modal nếu hết thời gian
-    }
-  }, 5000); // Kiểm tra mỗi 2 giây
         timeLeft -= 3; // Cập nhật thời gian còn lại (2 giây mỗi lần)
       } else {
         clearInterval(interval); // Hết thời gian, dừng việc kiểm tra
-        message.error("Thời gian thanh toán đã hết. Vui lòng thử lại.");
+        message.error(t("PAYMENT.PAY_QR_CODE_FAIL"));
         setIsOpenModalQRcode(false); // Đóng modal nếu hết thời gian
       }
     }, 5000); // Kiểm tra mỗi 2 giây
-
     setPaymentInterval(interval); // Lưu ID interval
   };
 
@@ -220,7 +212,7 @@ const handleQrCodePayment = () => {
         arrayOrdered.push(element.product);
       });
       dispatch(removeAllOrderProduct({ listChecked: arrayOrdered }));
-      message.success(t('PAYMENT.ORDER_SUCCESS'));
+      message.success(t("PAYMENT.ORDER_SUCCESS"));
       navigate("/order-success", {
         state: {
           delivery,
@@ -230,7 +222,7 @@ const handleQrCodePayment = () => {
         },
       });
     } else if (isError) {
-      message.error(t('PAYMENT.MESS_ERR_ORDER'));
+      message.error(t("PAYMENT.MESS_ERR_ORDER"));
     }
   }, [isSuccess, isError]);
 
@@ -387,30 +379,30 @@ const handleQrCodePayment = () => {
             <WrapperLeft>
               <WrapperInfo2>
                 <div>
-                  <Label>{t('PAYMENT.CHOOSE_DELIVERED_METHOD')}</Label>
+                  <Label>{t("PAYMENT.CHOOSE_DELIVERED_METHOD")}</Label>
                   <WrapperRadio onChange={handleDelivery} value={delivery}>
                     <Radio value="fast">
                       <span style={{ color: "#ea8500", fontWeight: "bold" }}>
                         FAST
                       </span>{" "}
-                      {t('PAYMENT.FAST_GO_JECT')}
+                      {t("PAYMENT.FAST_GO_JECT")}
                     </Radio>
                     <Radio value="gojek">
                       <span style={{ color: "#ea8500", fontWeight: "bold" }}>
                         GO_JEK
                       </span>{" "}
-                      {t('PAYMENT.FAST_GO_JECT')}
+                      {t("PAYMENT.FAST_GO_JECT")}
                     </Radio>
                   </WrapperRadio>
                 </div>
               </WrapperInfo2>
               <WrapperInfo3>
                 <div>
-                  <Label>{t('PAYMENT.CHOOSE_PAYMENT_METHOD')}</Label>
+                  <Label>{t("PAYMENT.CHOOSE_PAYMENT_METHOD")}</Label>
                   <WrapperRadio onChange={handlePayment} value={payment}>
-                    <Radio value="later_money">{t('PAYMENT.COD')}</Radio>
-                    <Radio value="paypal">{t('PAYMENT.PAYPAL')}</Radio>
-                    <Radio value="qr_code">{t('PAYMENT.QR_CODE')}</Radio>
+                    <Radio value="later_money">{t("PAYMENT.COD")}</Radio>
+                    <Radio value="paypal">{t("PAYMENT.PAYPAL")}</Radio>
+                    <Radio value="qr_code">{t("PAYMENT.QR_CODE")}</Radio>
                   </WrapperRadio>
                 </div>
               </WrapperInfo3>
@@ -420,7 +412,7 @@ const handleQrCodePayment = () => {
               <div style={{ width: "100%" }}>
                 <WrapperInfo1>
                   <div>
-                    <span>{t('PAYMENT.ADDRESS')}: </span>
+                    <span>{t("PAYMENT.ADDRESS")}: </span>
                     <span style={{ fontWeight: "bold" }}>
                       {`${user?.address} ${user?.city}`}{" "}
                     </span>
@@ -428,7 +420,7 @@ const handleQrCodePayment = () => {
                       onClick={handleChangeAddress}
                       style={{ color: "#9255FD", cursor: "pointer" }}
                     >
-                      {t('PAYMENT.CHANGE')}
+                      {t("PAYMENT.CHANGE")}
                     </span>
                   </div>
                 </WrapperInfo1>
@@ -440,7 +432,7 @@ const handleQrCodePayment = () => {
                       justifyContent: "space-between",
                     }}
                   >
-                    <span>{t('PAYMENT.SUBTOTAL')}</span>
+                    <span>{t("PAYMENT.SUBTOTAL")}</span>
                     <span
                       style={{
                         color: "#000",
@@ -458,7 +450,7 @@ const handleQrCodePayment = () => {
                       justifyContent: "space-between",
                     }}
                   >
-                    <span>{t('PAYMENT.DISCOUNT')}</span>
+                    <span>{t("PAYMENT.DISCOUNT")}</span>
                     <span
                       style={{
                         color: "#000",
@@ -476,7 +468,7 @@ const handleQrCodePayment = () => {
                       justifyContent: "space-between",
                     }}
                   >
-                    <span>{t('PAYMENT.DELIVERY_COST')}</span>
+                    <span>{t("PAYMENT.DELIVERY_COST")}</span>
                     <span
                       style={{
                         color: "#000",
@@ -489,7 +481,7 @@ const handleQrCodePayment = () => {
                   </div>
                 </WrapperInfo>
                 <WrapperTotal>
-                  <span>{t('PAYMENT.TOTAL')}</span>
+                  <span>{t("PAYMENT.TOTAL")}</span>
                   <span style={{ display: "flex", flexDirection: "column" }}>
                     <span
                       style={{
@@ -501,7 +493,7 @@ const handleQrCodePayment = () => {
                       {convertPrice(totalPriceMemo)}
                     </span>
                     <span style={{ color: "#000", fontSize: "11px" }}>
-                      {t('PAYMENT.VAT')}
+                      {t("PAYMENT.VAT")}
                     </span>
                   </span>
                 </WrapperTotal>
@@ -528,7 +520,7 @@ const handleQrCodePayment = () => {
                     border: "none",
                     borderRadius: "4px",
                   }}
-                  textbutton= {t('PAYMENT.PAY_WITH_QR')}
+                  textbutton={t("PAYMENT.PAY_WITH_QR")}
                   styletextbutton={{
                     color: "#fff",
                     fontSize: "15px",
@@ -546,7 +538,7 @@ const handleQrCodePayment = () => {
                     border: "none",
                     borderRadius: "4px",
                   }}
-                  textbutton={t('PAYMENT.ORDER_NOW')}
+                  textbutton={t("PAYMENT.ORDER_NOW")}
                   styletextbutton={{
                     color: "#fff",
                     fontSize: "15px",
@@ -558,7 +550,7 @@ const handleQrCodePayment = () => {
           </div>
         </div>
         <ModalQRcode
-          title={t('PAYMENT.TILE_QR_CODE')}
+          title={t("PAYMENT.TILE_QR_CODE")}
           open={isOpenModalQRcode}
           amount={totalPriceMemo} // Số tiền là 100,000 VND
           productName={content}
@@ -569,7 +561,7 @@ const handleQrCodePayment = () => {
         </ModalQRcode>
 
         <ModalComponent
-          title={t('PAYMENT.UPDATE_SHIPPING_ADDRESS')}
+          title={t("PAYMENT.UPDATE_SHIPPING_ADDRESS")}
           open={isOpenModalUpdateInfo}
           onCancel={handleCancelUpdate}
           onOk={handleUpdateInfoUser}
@@ -584,9 +576,11 @@ const handleQrCodePayment = () => {
               form={form}
             >
               <Form.Item
-                label={t('PAYMENT.NAME')}
+                label={t("PAYMENT.NAME")}
                 name="name"
-                rules={[{ required: true, message: t('PAYMENT.PLACEHOODER_NAME') }]}
+                rules={[
+                  { required: true, message: t("PAYMENT.PLACEHOODER_NAME") },
+                ]}
               >
                 <InputComponent
                   value={stateUserDetails["name"]}
@@ -595,9 +589,11 @@ const handleQrCodePayment = () => {
                 />
               </Form.Item>
               <Form.Item
-                label={t('PAYMENT.CITY')}
+                label={t("PAYMENT.CITY")}
                 name="city"
-                rules={[{ required: true, message: t('PAYMENT.PLACEHOODER_CITY')}]}
+                rules={[
+                  { required: true, message: t("PAYMENT.PLACEHOODER_CITY") },
+                ]}
               >
                 <InputComponent
                   value={stateUserDetails["city"]}
@@ -606,10 +602,10 @@ const handleQrCodePayment = () => {
                 />
               </Form.Item>
               <Form.Item
-                label={t('PAYMENT.PHONE')}
+                label={t("PAYMENT.PHONE")}
                 name="phone"
                 rules={[
-                  { required: true, message: t('PAYMENT.PLACEHOODER_PHONE')},
+                  { required: true, message: t("PAYMENT.PLACEHOODER_PHONE") },
                 ]}
               >
                 <InputComponent
@@ -620,10 +616,10 @@ const handleQrCodePayment = () => {
               </Form.Item>
 
               <Form.Item
-                label={t('PAYMENT.ADDRESS')}
+                label={t("PAYMENT.ADDRESS")}
                 name="address"
                 rules={[
-                  { required: true, message: t('PAYMENT.PLACEHOODER_ADDRESS')},
+                  { required: true, message: t("PAYMENT.PLACEHOODER_ADDRESS") },
                 ]}
               >
                 <InputComponent
